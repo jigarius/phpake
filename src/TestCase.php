3b5f3bd62849884d4f1dee7f583065c655dbc8e3
Jigar Mehta <hello@jigarius.com>
2021-10-08 14:04:31 -0400

    Create custom TestCase with helper methods

---
+++ src/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Phpake;
+
+use PHPUnit\Framework\TestCase as TestCaseBase;
+
+/**
+ * Phpake Test Case.
+ */
+class TestCase extends TestCaseBase {
+
+  /**
+   * Creates a temporary file with the given contents.
+   *
+   * @param string $data
+   *   Contents to write to the file.
+   * @return string
+   *   Path to the file.
+   */
+  protected static function createTempFile(string $data): string {
+    $path = tempnam(sys_get_temp_dir(), 'Phpake.');
+    file_put_contents($path, $data);
+    return $path;
+  }
+
+}
