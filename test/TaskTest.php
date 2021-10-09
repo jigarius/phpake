49e5d41765741d193458d8996227ba002a48ef32
Jigar Mehta <hello@jigarius.com>
2021-10-09 01:27:37 -0400

    Organize tests and use PhpakeFile::require()

--- test/TaskTest.php
+++ test/TaskTest.php
@@ -1,15 +1,13 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
 use Phpake\Task;
 use Phpake\Argument\Argument;
-
-require_once __DIR__ . '/../examples/hello-world.phpakefile';
-require_once __DIR__ . '/../examples/input-output.phpakefile';
-require_once __DIR__ . '/../examples/fizzbuzz.phpakefile';
+use Phpake\TestCase;
+use Phpake\PhpakeFile;
 
 /**
  * @covers \Phpake\Task
+ * @uses \Phpake\PhpakeFile
  * @uses \Phpake\Argument\Argument
  * @uses \Phpake\Argument\BuiltInArgument
  */
@@ -22,25 +20,39 @@ class TaskTest extends TestCase {
   protected Task $helloHuman;
 
   public function setUp(): void {
+    // ReflectionFunction objects can only be created with real functions.
+    // Loading these definitions with Phpake::require() makes sure that the
+    // callbacks are identified properly.
+    PhpakeFile::require(__DIR__ . '/../examples/hello-world.phpakefile');
+    PhpakeFile::require(__DIR__ . '/../examples/input-output.phpakefile');
+
+    // Has no annotations.
     $this->helloLadies = new Task(new ReflectionFunction('hello_ladies'));
+
+    // Has just a summary.
     $this->helloWorld = new Task(new ReflectionFunction('hello_world'));
+
+    // Has detailed annotations.
     $this->helloHuman = new Task(new ReflectionFunction('Input_Output\\hello_human'));
   }
 
   public function testGetCallback() {
     $this->assertEquals('hello_world', $this->helloWorld->getCallback());
+  }
+
+  public function testGetCallbackWithNamespace() {
     $this->assertEquals('Input_Output\\hello_human', $this->helloHuman->getCallback());
   }
 
   public function testGetSummary() {
-    $this->assertEquals('', $this->helloLadies->getSummary());
-    $this->assertEquals('Say hello world.', $this->helloWorld->getSummary());
     $this->assertEquals('Say hello to a human.', $this->helloHuman->getSummary());
   }
 
+  public function testGetSummaryWithoutDocBlock() {
+    $this->assertEquals('', $this->helloLadies->getSummary());
+  }
+
   public function testGetDescription() {
-    $this->assertEquals('', $this->helloLadies->getDescription());
-    $this->assertEquals('', $this->helloWorld->getDescription());
     $this->assertEquals(
       <<<EOD
 Uses a required parameter, and an optional parameter. "param" annotations
@@ -53,14 +65,28 @@ EOD,
     );
   }
 
+  public function testGetDescriptionWithoutDocBlock() {
+    $this->assertEquals('', $this->helloLadies->getDescription());
+  }
+
+  public function testGetDescriptionEmpty() {
+    $this->assertEquals('', $this->helloWorld->getDescription());
+  }
+
   public function testGetCommand() {
     $this->assertEquals('hello-world', $this->helloWorld->getCommand());
+  }
+
+  public function testGetCommandWithNamespace() {
     $this->assertEquals('input-output:hello-human', $this->helloHuman->getCommand());
   }
 
   public function testGetUsages() {
-    $this->assertEquals([], $this->helloWorld->getUsages());
     $this->assertEquals(['Niki', 'Niki Martin'], $this->helloHuman->getUsages());
+  }
+
+  public function testGetUsagesEmpty() {
+    $this->assertEquals([], $this->helloWorld->getUsages());
   }
 
   public function testIsHidden() {
@@ -68,21 +94,31 @@ EOD,
 
     $task = new Task(new ReflectionFunction('_this_is_not_a_task'));
     $this->assertTrue($task->isHidden());
+  }
 
-    $task = new Task(new ReflectionFunction('Fizzbuzz\\_fizzbuzz'));
+  public function testIsHiddenWithNamespace() {
+    $task = new Task(new ReflectionFunction('Input_Output\\_do_nothing'));
     $this->assertTrue($task->isHidden());
   }
 
   public function testGetArguments() {
-    $this->assertEquals([], $this->helloWorld->getArguments());
-    $this->assertEquals([
-      'fname' => Argument::create('fname', 'First name.', FALSE, NULL),
-      'lname' => Argument::create('lname', 'Last name.', TRUE, NULL),
-      'output' => Argument::create('output', '', TRUE, NULL),
-    ],
-    $this->helloHuman->getArguments());
+    $this->assertEquals(
+      [
+        'fname' => Argument::create('fname', 'First name.', FALSE, NULL),
+        'lname' => Argument::create('lname', 'Last name.', TRUE, NULL),
+        'output' => Argument::create('output', '', TRUE, NULL),
+      ],
+      $this->helloHuman->getArguments()
+    );
   }
 
+  public function testGetArgumentsEmpty() {
+    $this->assertEquals([], $this->helloWorld->getArguments());
+  }
+
+  /**
+   * Task::getArguments() doesn't recompute argument data.
+   */
   public function testGetArgumentsCachesResults() {
     $reflection = $this->getMockBuilder(\ReflectionFunction::class)
       ->setConstructorArgs(['Input_Output\\hello_human'])
@@ -94,6 +130,7 @@ EOD,
 
     $task = new Task($reflection);
     $task->getArguments();
+    // The second call gets the arguments from memory.
     $task->getArguments();
   }
 
