39547327518fda7dfd7fd29706a3a7d8e2c3e3ed
Jerry Radwick <r2d2@jigarius.com>
2025-01-12 18:35:55 -0500

    Use PHP 8.3

--- src/Commands/ExecCommand.php
+++ src/Commands/ExecCommand.php
@@ -51,7 +51,7 @@ class ExecCommand extends Command {
     $this->task = $task;
   }
 
-  protected function execute(InputInterface $input, OutputInterface $output) {
+  protected function execute(InputInterface $input, OutputInterface $output): int {
     $values = $input->getArguments();
     $values['input'] = $input;
     $values['output'] = $output;
